2b625caecb8c0b9cae64adc048992a73846d2936
kunitahayashi <79304693+kunitahayashi@users.noreply.github.com>
2021-03-16 17:59:48 +0900

    商品出品機能fix2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,21 +19,10 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def update
-  #   @user = User.find(params[:id])
-  #   if @user.update(users_params)
-  #     redirect_to root_path
-  #   else
-  #     render :update
-  #   end
-  # end     今後使う可能性ある為
-
-
   private
 
   def items_params
     params.require(:item).permit(:image, :price, :item, :explanation, :category_id, :status_id, :fee_id, :address1_id, :day_id).merge(user_id: current_user.id)
-    # .merge(user_id: current_user.id)許可したデータに追加でuserテーブルのデータを紐付ける
   end
 end
 
