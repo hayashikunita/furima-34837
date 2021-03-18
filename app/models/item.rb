13203ed4dc7500a5f6744ab901b67c6b9db8f6db
kunitahayashi <79304693+kunitahayashi@users.noreply.github.com>
2021-03-18 19:42:47 +0900

    商品削除機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,36 +1,29 @@
 class Item < ApplicationRecord
-
-
-
-  #アクティブハッシュの際に必要になる記述 
+  # アクティブハッシュの際に必要になる記述
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :address1
   belongs_to :status
   belongs_to :fee
   belongs_to :day
   belongs_to :category
-  #アクティブハッシュの際に必要になる記述 
+  # アクティブハッシュの際に必要になる記述
 
-
-
-  has_one_attached :image 
+  has_one_attached :image
 
   with_options presence: true do
-  validates :image
-  validates :item
-  validates :explanation
-  validates :category_id , numericality: { other_than: 1 } 
-  validates :status_id, numericality: { other_than: 1 } 
-  validates :fee_id, numericality: { other_than: 1 } 
-  validates :address1_id, numericality: { other_than: 1 } 
-  validates :day_id, numericality: { other_than: 1 } 
+    validates :image
+    validates :item
+    validates :explanation
+    validates :category_id, numericality: { other_than: 1 }
+    validates :status_id, numericality: { other_than: 1 }
+    validates :fee_id, numericality: { other_than: 1 }
+    validates :address1_id, numericality: { other_than: 1 }
+    validates :day_id, numericality: { other_than: 1 }
   end
-
 
   validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
   validates :price,
             numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
- 
 
   has_one    :purchase
   belongs_to :user
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,18 +6,11 @@ RSpec.describe Item, type: :model do
       @item = FactoryBot.build(:item)
     end
 
-
-
     context '登録に成功する時' do
-
       it '入力欄を全て埋めれば登録できること' do
         expect(@item).to be_valid
       end
-
-
     end
-
-
 
     context '登録に失敗する時' do
       it 'imageは空では登録できないこと' do
@@ -29,25 +22,25 @@ RSpec.describe Item, type: :model do
       it 'priceが空では登録できないこと' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price Out of setting range")
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
       end
 
       it 'priceが全角数字では登録できないこと' do
         @item.price = '１１１１'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price Half-width number")
+        expect(@item.errors.full_messages).to include('Price Half-width number')
       end
 
       it '半角英数混合では登録できないこと' do
         @item.price = '12aa'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price Half-width number")
+        expect(@item.errors.full_messages).to include('Price Half-width number')
       end
-      
+
       it '半角英語だけでは登録できないこと' do
         @item.price = 'aaa'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price Half-width number")
+        expect(@item.errors.full_messages).to include('Price Half-width number')
       end
 
       it 'itemは空では登録できないこと' do
@@ -62,37 +55,37 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
 
-      it "category_idが1の場合は登録できないこと" do
+      it 'category_idが1の場合は登録できないこと' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category must be other than 1")
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
 
-      it "status_idが1の場合は登録できないこと" do
+      it 'status_idが1の場合は登録できないこと' do
         @item.status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Status must be other than 1")
+        expect(@item.errors.full_messages).to include('Status must be other than 1')
       end
 
-      it "fee_idが1の場合は登録できないこと" do
+      it 'fee_idが1の場合は登録できないこと' do
         @item.fee_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Fee must be other than 1")
+        expect(@item.errors.full_messages).to include('Fee must be other than 1')
       end
 
-      it "address1_idが1の場合は登録できないこと" do
+      it 'address1_idが1の場合は登録できないこと' do
         @item.address1_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Address1 must be other than 1")
+        expect(@item.errors.full_messages).to include('Address1 must be other than 1')
       end
 
-      it "day_idが1の場合は登録できないこと" do
+      it 'day_idが1の場合は登録できないこと' do
         @item.day_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Day must be other than 1")
+        expect(@item.errors.full_messages).to include('Day must be other than 1')
       end
 
-      it "販売価格が¥300より少ない時は出品できないこと" do
+      it '販売価格が¥300より少ない時は出品できないこと' do
         @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price Out of setting range')
