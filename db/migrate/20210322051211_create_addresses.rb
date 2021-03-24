a34254d354de883a021e2e63a5d05642ddd37f8f
kunitahayashi <79304693+kunitahayashi@users.noreply.github.com>
2021-03-24 19:59:14 +0900

    S3に保存する処理を実行しようfix2

--- db/migrate/20210322051211_create_addresses.rb
+++ db/migrate/20210322051211_create_addresses.rb
@@ -5,7 +5,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.integer :address1_id,        null: false
       t.string :shikucyouson,        null: false
       t.string :bannchi,             null: false
-      t.string :tatemonomei,
+      t.string :tatemonomei
       t.string :phone_number,        null: false
       t.references :purchase,        foreign_key: true
       t.timestamps
